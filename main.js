716990833474ddb50ef13baa21721827db370bfd
Zunon <zunon@zunon.io>
2018-05-17 01:37:33 +0400

    Basic role handling

--- main.js
+++ main.js
@@ -6,9 +6,31 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content.startsWith('!joinchannel')) {
+    var channelName = msg.content.split(' ')[1];
+    role = msg.guild.roles.find('name', channelName);
+    msg.member
+      .addRole(role)
+      .then(updated => {
+        console.log('Added ' + channelName + ' for ' + msg.member.displayName);
+        msg.reply('Added you to the `' + channelName + '` channel!');
+      },
+      error => {
+        console.error('could not add ' + channelName + ' for ' + msg.member.displayName + '!');
+        msg.reply('Could not find that channel!');
+      });
+  }
+  if (msg.content.startsWith('!<>doRoles')) {
+    var channelName = msg.content.split(' ')[1];
+    role = msg.guild.roles.find('name', channelName);
+    channel = msg.guild.channels.find('name', channelName);
+    channel.overwritePermissions(role, {READ_MESSAGES: true})
+      .catch(error => cosole.error)
+      .then(updated => {
+        console.log('Did permissions for: ' + channelName);
+      }); 
   }
 });
 
-client.login('NDQ2NDEwODkwNTY5MzE4NDAw.Dd4oEQ.yCm3lDUQuEdWVAeyFyhWZiu3Djg');
+client.login('NDQ2NDEwODkwNTY5MzE4NDAw.Dd4oEQ.yCm3lDUQuEdWVAeyFyhWZiu3Djg')
+  .catch(error => console.error);
