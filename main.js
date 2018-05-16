88307e9f9752bc0699bc41ab16d5eed965ff2a9a
Zunon <zunon@zunon.io>
2018-05-17 03:27:32 +0400

    Add capability to join, leave, or manage multiple channels at once

--- main.js
+++ main.js
@@ -1,49 +1,73 @@
-const Discord = require('discord.js');
+const Discord = require(`discord.js`);
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on(`ready`, () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  var text = msg.content.toLowerCase();
-  if (text.startsWith('!joinchannel')) {
-    var channelName = text.split(' ')[1];
-    role = msg.guild.roles.find('name', channelName);
-    msg.member
-      .addRole(role)
-      .then(updated => {
-        console.log('Added ' + channelName + ' for ' + msg.member.displayName);
-        msg.reply('Added you to the `' + channelName + '` channel!');
-      },
-      error => {
-        console.error('could not add ' + channelName + ' for ' + msg.member.displayName + '!');
-        msg.reply('Could not find that channel!');
-      });
-  } else if (text.startsWith('!<>doRoles')) {
-    var channelName = text.split(' ')[1];
-    role = msg.guild.roles.find('name', channelName);
-    channel = msg.guild.channels.find('name', channelName);
-    channel.overwritePermissions(role, {READ_MESSAGES: true})
-      .catch(error => console.error)
-      .then(updated => {
-        console.log('Did permissions for: ' + channelName);
-      }); 
-  } else if(text.startsWith('!leavechannel')) {
-    var channelName = text.split(' ')[1];
-    role = msg.guild.roles.find('name', channelName);
-    msg.member
-      .removeRole(role)
-      .then(updated => {
-        console.log('Removed ' + channelName + ' for ' + msg.member.displayName);
-        msg.reply('Removed you from the `' + channelName + '` channel!');
-      },
-      error => {
-        console.error('could not remove ' + channelName + ' for ' + msg.member.displayName + '!');
-        msg.reply('Could not find that channel!');
-      });
+client.on(`message`, msg => {
+  var text = msg.content.toLowerCase().split(` `);
+  var command = text[0];
+  text.shift();
+  
+  switch(command) {
+    case `!joinchannel`:
+      joinChannel(msg, text);
+      break;
+    case `!leavechannel`:
+      leaveChannel(msg, text);
+      break;
+    case `!doroles`:
+      doRoles(msg, text);
+      break;
   }
 });
 
-client.login('NDQ2NDEwODkwNTY5MzE4NDAw.Dd4oEQ.yCm3lDUQuEdWVAeyFyhWZiu3Djg')
+client.login(`NDQ2NDEwODkwNTY5MzE4NDAw.Dd4oEQ.yCm3lDUQuEdWVAeyFyhWZiu3Djg`)
   .catch(error => console.error);
+
+function joinChannel(message, text) {
+  text.forEach(channelName => {
+    var role = message.guild.roles.find(`name`, channelName);
+    message.member
+      .addRole(role)
+      .then(updated => {
+        console.log(`Added ${channelName} for ${message.member.displayName}`);
+        msg.reply(`Added you to the \`${channelName}\` channel!`);
+      },
+      error => {
+        console.error(`could not add ${channelName} for ${message.member.displayName}!`);
+        msg.reply(`Could not find that channel!`);
+      });
+  });
+}
+
+function leaveChannel(message, text) {
+  text.forEach( channelName => {
+    role = msg.guild.roles.find(`name`, channelName);
+    msg.member
+      .removeRole(role)
+
+      .then(updated => {
+        console.log(`Removed ${channelName} for ${message.member.displayName}`);
+        msg.reply(`removed you from the \`${channelName}\` channel!`);
+      },
+
+      error => {
+        console.error(`could not remove ${channelName} for ${msg.member.displayName}!`);
+        msg.reply(`could not find that channel!`);
+      });
+  });
+}
+
+function doRoles(message, text) {
+  text.forEach(channelName => {
+    role = msg.guild.roles.find(`name`, channelName);
+    channel = msg.guild.channels.find(`name`, channelName);
+    channel.overwritePermissions(role, {READ_MESSAGES: true})
+      .catch(error => console.error)
+      .then(updated => {
+        console.log(`Did permissions for: ${channelName}`);
+      }); 
+  })
+}
