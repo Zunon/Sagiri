8d2cc37dec4df8af5ed446a9293f3f088fed3d2d
Zunon <zunon@zunon.io>
2018-05-21 10:33:06 +0400

    Add basic documentation and TODOs, change npm start command

--- handlers.ts
+++ handlers.ts
@@ -1,16 +1,26 @@
+/*
+  ------------ IMPORTS, INSTANCES, AND VARIABLES ------------
+*/
 import { Message, Client, Channel, CategoryChannel, TextChannel, Role, Collection, GuildMember, Guild } from "discord.js"
-
-export function joinChannel(message: Message, text: string[]) {
-  text.forEach((channelName: string) => {
-    let role = message.guild.roles.find(`name`, channelName)
-    message.member
-      .addRole(role)
+/*
+  ------------ FUNCTIONS ------------
+*/
+/**
+ * Add requested channel-role(s) to the user who sent the message
+ * @param message Message object that contained the command
+ * @param channels Array of requested channel names
+ */
+export function joinChannel(message: Message, channels: string[]) {
+  channels.forEach((channelName: string) => { // Iterate over requested channels
+    let role: Role = message.guild.roles.find(`name`, channelName) // Find the needed role
+    message.member // Get the GuildMember object
+      .addRole(role) // Add the needed role to it
       .then(
-        () => {
+        () => { // If role addition was a success, print out on console and reply the confirmation
           console.log(`Added ${channelName} for ${message.member.displayName}`)
           message.reply(`Added you to the \`${channelName}\` channel!`)
         },
-        () => {
+        () => { // If role addition was a failure, print out on console and reply the (un)confirmation(?)
           console.error(`could not add ${channelName} for ${message.member.displayName}!`)
           message.reply(`Could not find that channel!`)
         }
@@ -18,18 +28,22 @@ export function joinChannel(message: Message, text: string[]) {
     }
   )
 }
-
-export function leaveChannel(message: Message, text: string[]) {
-  text.forEach((channelName: string) => {
-    let role: Role = message.guild.roles.find(`name`, channelName)
-    message.member
-      .removeRole(role)
+/**
+ * Remove requested channel-role(s) to the user who sent the message
+ * @param message Message object that contained the command
+ * @param channels Array of requested channel names
+ */
+export function leaveChannel(message: Message, channels: string[]) {
+  channels.forEach((channelName: string) => { // Iterate over requested channels
+    let role: Role = message.guild.roles.find(`name`, channelName) // Find the needed role
+    message.member // Get the GuildMember object
+      .removeRole(role) // Remove the needed role from it
       .then(
-        () => {
+        () => { // If role removal was a success, print out on console and reply the confirmation
           console.log(`Removed ${channelName} for ${message.member.displayName}`)
           message.reply(`removed you from the \`${channelName}\` channel!`)
         },
-        () => {
+        () => { // If role removal was a failure, print out on console and reply the (un)confirmation(?)
         console.error(`could not remove ${channelName} for ${message.member.displayName}!`)
         message.reply(`could not find that channel!`)
         }
@@ -37,13 +51,20 @@ export function leaveChannel(message: Message, text: string[]) {
     }
   )
 }
-
+/**
+ * Re-create roles for the "ONGOING DISCUSSION" channel category
+ * @param message Message object that contained the command
+ * @param client Client object for the bot
+ */
 export function doRoles(message: Message, client: Client) {
-  var guild = message.guild
-  deleteAllRoles(guild)
+  var guild = message.guild // get the Guild object
+  deleteAllRoles(guild) // delete all existing roles to prevent role duplication
+  // Get the CategoryChannel object for the "ONGOING DISCUSSION" channel category
   let ongoingChannelCategory: CategoryChannel = client.channels.get(`442729303935811584`) as CategoryChannel
-  ongoingChannelCategory.children.forEach((channel: TextChannel) => {
-    guild.createRole({ name: channel.name }).then((role: Role) => {
+  ongoingChannelCategory.children.forEach((channel: TextChannel) => { // Iterate over the TextChannels inside it
+    // Create a role that has the same name as the channel
+    guild.createRole({ name: channel.name }).then((role: Role) => { // If role creation is a success
+      // Allow members of that role to view the channel, print out the result to the console
       channel.overwritePermissions(role, { READ_MESSAGES: true })
         .then(
           () => {
@@ -58,24 +79,36 @@ export function doRoles(message: Message, client: Client) {
     }
   )
 }
-
+/**
+ * Prune a specific number of previous messages
+ * @param channel TextChannel object of the channel that will be pruned
+ * @param numberOfMessages Number of messages that will be pruned
+ */
 export function prune(channel: TextChannel, numberOfMessages: number) {
   channel.fetchMessages({ limit: numberOfMessages }).then((messages: Collection<string, Message>) => {
     messages.forEach((message: Message) => {
       message.delete()
     })
-    channel.send(`Successfully pruned ${numberOfMessages - 1} messages!`)
+    channel.send(`Pruning ${numberOfMessages - 1} messages!`)
       .then((reply: Message) => {
         reply.delete(3000)
       }
     )
   })
 }
-
+/**
+ * Check that a user is allowed to use elevated commands
+ * @param user User object that will be authenticated
+ * @todo Check for unterfuhrers and fuhrers as well
+ */
 export function authenticate(user: GuildMember) {
   return user.id === '95623672072511488'
 }
-
+/**
+ * Delete all roles in a guild
+ * @param guild Guild object
+ * @todo Only delete roles that relate to "ONGOING DISCUSSION" channel category
+ */
 function deleteAllRoles(guild: Guild) {
   guild.roles.forEach(role => {
     role.delete()
--- main.ts
+++ main.ts
@@ -1,23 +1,49 @@
-import { Client, Message, TextChannel } from "discord.js"
-import { joinChannel, leaveChannel, doRoles, prune, authenticate } from "./handlers"
-
+/*
+  ------------ IMPORTS, INSTANCES, AND VARIABLES ------------
+*/
+import { Client, Message, TextChannel } from "discord.js" // Import needed classes from discord.js library
+import { joinChannel, leaveChannel, doRoles, prune, authenticate } from "./handlers" // import methods from my handler library
+// Instantiate a client to use it
 const client: Client = new Client()
-
+/*
+  ------------ LISTENERS ------------
+*/
+/**
+ * Spits out unhandled errors without panicking
+ * @listens error
+ * @todo Replace it to handle specific error events as they arise
+ */
 client.on(`error`, console.error)
-
+/**
+ * Parses commands and fires the appropriate handler
+ * @listens message
+ */
 client.on(`message`, (message: Message) => {
+  // Check if message is a command
   if(message.content.startsWith('!')) {
+    // Splits message into command and arguments
     var text: string[] = message.content.toLowerCase().split(` `)
     var command: string = text[0].substr(1)
     text.shift()
-    
+    // Matches command with known commands
     switch(command) {
+      /*
+        If it's a public command, it fires the handler straight away
+        and passes the message object and the arguments passed into
+        the command
+      */
       case `joinchannel`:
         joinChannel(message, text)
         break
       case `leavechannel`:
         leaveChannel(message, text)
         break
+      /*
+        If it's a command that requires elevation, it first checks
+        that the user who sent the command passes the authentication
+        function, and then it fires the handler with the appropriate
+        arguments
+      */
       case `doroles`:
         if (authenticate(message.member)) {
           doRoles(message, client)
@@ -32,16 +58,26 @@ client.on(`message`, (message: Message) => {
           message.reply('you must be an authorized user to use this command!')
         }
         break
+      // If the command wasn't recognized, it replies saying that the command wasn't recognized
       default:
         message.reply("sorry I didn't recognize that command.")
     }
   }
 })
-
+/**
+ * As soon as the bot is up and ready, confirms to console
+ * @listens ready
+ */
 client.on(`ready`, () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
-
+/**
+ * @todo Add event handlers to handle reactionAdd and reactionRemove
+ */
+/*
+  ------------ EXECUTIONS ------------
+*/
+// Attempt to login
 client.login(`NDQ2NDEwODkwNTY5MzE4NDAw.Dd4oEQ.yCm3lDUQuEdWVAeyFyhWZiu3Djg`)
   .catch(error => {
     console.error(`Couldn't log in!\n${error}`)
